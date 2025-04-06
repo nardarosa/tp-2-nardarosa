0fce6c3ab57fa443dc1b9e101faacc0b1371927b
nardarosa <nardarosa2050@gmail.com>
2025-04-06 12:33:05 -0300

    tp 2

--- change.py
+++ change.py
@@ -1,3 +1,11 @@
 def change():
     expense = 23.75
     money = 100
+    vuelto = str(money - expense)
+    position = vuelto.find(".")
+    pesos = vuelto[:position]
+    print(f"pesos {pesos}")
+    centavos = vuelto[position + 1:]
+    print(f"centavos {centavos}")
+
+change()
