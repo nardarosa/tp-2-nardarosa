65a2afc708da41b9e8d8eb3b7a5113a4e6f7f5b9
nardarosa <nardarosa2050@gmail.com>
2025-04-07 09:45:10 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,8 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+    print(first_name.lower() + " " + last_name.lower())
+    print(first_name[:1].upper() + first_name[1:].lower() + " " + last_name[:1].upper() + last_name[1:].lower())
+    print(first_name.upper() + " " + last_name.upper())
+    print("\t" + first_name.lower() + " " + last_name.lower())
+ada()
